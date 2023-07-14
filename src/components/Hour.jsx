1125412d0d4d4366a4c1a88a91c96b7a38c9f5e5
Max <max.wolts@gmail.com>
2023-07-14 07:37:51 -0300

    :tada: start

---
+++ src/components/Hour.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const Hour = () => {
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const date = new Date();
+      const options = {
+        timeZone: "America/Argentina/Buenos_Aires",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      };
+      const formattedTime = date.toLocaleTimeString([], options);
+      setTime(formattedTime);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <p>{time}ART</p>
+    </>
+  );
+};
+
+export default Hour;
---
+++ src/components/Title.jsx
@@ -0,0 +1,15 @@
+import Hours from "./Hour"
+
+const Title = () => {
+  return (
+    <div>
+        <h1>Article</h1>
+        <div>
+            <Hours/>
+            <h2 className="text">Brand and Digital for Culture, Commerce and Good.</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Title
