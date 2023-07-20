1cda3439b9ad27b41092c2ae069d11bd0f150736
MaxWolts <max.wolts@gmail.com>
2023-07-20 03:45:07 -0300

    :construction: Temporary example image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Title from './components/Title/Title'
+import exampleImage from './assets/example.jpg'
 
 function App() {
 
   return (
     <div className='bg-slate-950'>
       <Title />
+      <div >
+        <img src={exampleImage} alt="main image" className='w-screen'/>
+      </div>
     </div>
   )
 }
